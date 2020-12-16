8fe2520da2c97b0559ffa33e48d5b0a83092b03a
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2020-12-15 23:24:33 -0500

    added washing gauge

--- static/js/app.js
+++ static/js/app.js
@@ -92,6 +92,34 @@ d3.json("static/data/samples.json").then((data) => {
     Plotly.newPlot('bubble', [bubbleTrace], layout);
 
     // Generate gauge chart
+    var gaugeTrace = {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: filteredMetadata[0].wfreq,
+        title: { text: "Wash Frequency" },
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { range: [null, 9] },
+          bar: {color: '#1eb6a7'},
+          steps: [
+            { range: [0, 1], color: '#FF0080' },
+            { range: [1, 2], color: '#E71C8A' },
+            { range: [2, 3], color: '#CF3894' },
+            { range: [3, 4], color: '#B7549E' },
+            { range: [4, 5], color: '#9F70A8' },
+            { range: [5, 6], color: '#878CB2' },
+            { range: [6, 7], color: '#6FA8BC' },
+            { range: [7, 8], color: '#57C4C6' },
+            { range: [8, 9], color: '#40E0D0' },
+          ],
+          text: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', ''],
+          textposition: 'inside'
+        }
+      };
+
+    var gaugeLayout = {height: 500, width: 600};
+
+    Plotly.newPlot('gauge', [gaugeTrace], gaugeLayout);
 
 });
 
